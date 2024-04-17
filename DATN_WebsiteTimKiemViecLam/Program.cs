2ec5b28bee974fdc2783959107d02bb677fb1b44
Nguyễn Hoài <hoainhoc101@gmail.com>
2024-04-17 17:38:03 +0700

    cmt admin

--- DATN_WebsiteTimKiemViecLam/Program.cs
+++ DATN_WebsiteTimKiemViecLam/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Doanhnghiep}/{action=hienthidanhsachbaidang}");
+    pattern: "{controller=Taikhoan}/{action=btnHienthidanhsachtaikhoan}");
 
 app.Run();
